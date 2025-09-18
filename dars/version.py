ab2edae2315ce50e68a3ee96445de67a9a9e8847
ZtaDev <85247633+ZtaMDev@users.noreply.github.com>
2025-09-17 20:09:56 -0500

    1.1.5 tests Bugfixes

--- dars/version.py
+++ dars/version.py
@@ -1,2 +1,2 @@
-__version__ = "1.1.3"
-__release_url__ = "https://github.com/ZtaMDev/Dars-Framework/releases/tag/1.1.3"
+__version__ = "1.1.5"
+__release_url__ = "https://github.com/ZtaMDev/Dars-Framework/releases/tag/1.1.5"
